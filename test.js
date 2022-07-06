d56df9264ec52fb250032e4676802b9e858fed9d
Jacob Britton <jacobbritton95@gmail.com>
2022-07-05 20:30:41 -0400

    Remove extra JS file

--- test.js
+++
@@ -1,8 +0,0 @@
-const myLibrary = [];
-
-const button = document.querySelector('#add-book-button');
-button.addEventListener('click', addNewBook);
-
-function addNewBook () {
-    myLibrary.push(2)
-}
